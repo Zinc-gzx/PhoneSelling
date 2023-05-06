06fac3992f2e9cc11044f4cf5ed9b4455084b089
Zhixin Gao <zgao9758@uni.sydney.edu.au>
2023-05-06 23:34:48 +1000

    a

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
-import SearchBar from './components/SearchBar';
 import NavBar from './components/NavBar.js';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from './pages/home/home';
+import { Checkout } from './pages/checkout/checkout';
 function App() {
 
   return <div className='App'>
     <div className='header-container'>
-    <h1>OldPhoneDeals</h1>
+    <h1>Old Phone Deals</h1>
     </div>
-    <NavBar />
+    <Router>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+    </Router>
   
       
   </div>
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,6 +6,8 @@ import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import SearchBar from './SearchBar';
+import { Link } from 'react-router-dom';
+import { ShoppingCart } from 'phosphor-react';
 
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -14,13 +16,14 @@ const NavBar = () => {
   return (
     <Navbar className='NavBar'>
       <Container>
-        <Row>
-        <SearchBar />
+      <Row>
+        <Link to="/"><SearchBar /></Link>
         </Row>
         <Row>
           <Col>
-          <Button variant="secondary">Checkout</Button>
           <SignInButtons />
+          <Link to = "/checkout"><Button variant="secondary">Checkout</Button></Link>
+          <Link to = "/checkout"> <ShoppingCart size={32}/></Link>
           </Col>   
         </Row>
       </Container>
